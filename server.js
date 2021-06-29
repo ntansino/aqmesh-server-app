35b858b10c3cc7a4770a858b436db6a1cbc1f6ad
ntansino <ntansino@gmail.com>
2021-06-29 16:09:29 -0400

    Redirect partially implemented. Needs more work done in server.js (EOD 06-29-2021)

--- server.js
+++ server.js
@@ -1,12 +1,11 @@
 // Enable express and express-handlebars in project
 const express = require("express");
 const exphbs = require("express-handlebars");
+const bodyParser = require('body-parser');
 
 const app = express();
 const PORT = process.env.PORT || 1337;
 const PATH = require("path");
-
-
 
 // Configure template engine and template file (main.hbs)
 app.engine("hbs", exphbs({
@@ -17,14 +16,40 @@ app.engine("hbs", exphbs({
 // Set template engine
 app.set("view engine", "hbs");
 
-app.use(express.static(PATH.join(__dirname, "/assets")))
+// Give node access to everything in assets directory
+app.use(express.static(PATH.join(__dirname, "/assets")));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 // Route rendering
-app.get("/", (req, res) => {
-  res.render("home");
+app.get("/", function(req, res) {
+  res.render("home", { title: "Home Page - AQMesh API" });
 });
 
-app.get("/*", (req,res) => {
+/*
+app.get('/index', function (req, res) {
+    res.redirect('/');
+});
+*/
+
+app.post("/test", function(req, res) {
+
+  //var accountID = req.body.accountID;
+  const licenseKey = req.body.licenseKey;
+
+  console.log(licenseKey);
+
+  if(licenseKey != "\0") {
+    res.render("test", { title: "Test Page - AQMesh API" });
+    console.log("Redirected to /test");
+  }
+  else {
+    res.render("404", { title: "INCORRECT INFO" });
+    console.log("INCORRECT INFO");
+  }
+});
+
+app.get("/*", function(req, res) {
   res.render("404");
 });
 
@@ -32,23 +57,3 @@ app.get("/*", (req,res) => {
 app.listen(PORT, () => {
   console.log("Listening on localhost:" + PORT + "\n\n" + "Press Ctrl+C to quit..")
 });
-
-/*
-const router = express.Router();
-
-const PORT = process.env.PORT || 1337;
-
-const assetsDir = __dirname + "/public/"
-const indexDir = __dirname + "/public/html/";
-const styleDir = __dirname + "/public/css/";
-
-router.get('/', function(request, response) {
-  response.sendFile(PATH.join(indexDir + "index.html"));
-});
-
-app.use('/', router);
-app.use(express.static(__dirname + 'public'));
-app.listen(PORT);
-
-console.log("Listening on localhost:" + PORT + "\n\n" + "Press Ctrl+C to quit..");
-*/
