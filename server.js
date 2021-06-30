f358e4a405dd200560db7cad5e5e1dc989dfe87a
ntansino <ntansino@gmail.com>
2021-06-30 16:03:24 -0400

    EOD 06-30-2021

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
-// Enable express and express-handlebars in project
+// Enable NodeJS packages in project
 const express = require("express");
 const exphbs = require("express-handlebars");
 const bodyParser = require('body-parser');
-
-const app = express();
-const PORT = process.env.PORT || 1337;
 const PATH = require("path");
+
+// JSON object parsing
+const jsonParser = bodyParser.json();
+
+// Create Express instance
+const app = express();
+
+// Acquire custom localhost port number
+const PORT = process.env.PORT || 1337;
+
+
 
 // Configure template engine and template file (main.hbs)
 app.engine("hbs", exphbs({
@@ -16,44 +24,68 @@ app.engine("hbs", exphbs({
 // Set template engine
 app.set("view engine", "hbs");
 
-// Give node access to everything in assets directory
+// Shorten paths for all project subdirectories
 app.use(express.static(PATH.join(__dirname, "/assets")));
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+app.use(express.static(PATH.join(__dirname, "/assets/css")));
+app.use(express.static(PATH.join(__dirname, "/assets/img")));
+app.use(express.static(PATH.join(__dirname, "/assets/js")));
+app.use(express.static(PATH.join(__dirname, "/views")));
+app.use(express.static(PATH.join(__dirname, "/views/layouts")));
 
-// Route rendering
+// Give access to necessary middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+// ------------------------ ROUTES ------------------------
 app.get("/", function(req, res) {
   res.render("home", { title: "Home Page - AQMesh API" });
 });
 
-/*
-app.get('/index', function (req, res) {
-    res.redirect('/');
-});
-*/
-
 app.post("/test", function(req, res) {
 
-  //var accountID = req.body.accountID;
+  // Acquire values from HTML form input
+  const accountID = req.body.accountID;
   const licenseKey = req.body.licenseKey;
 
-  console.log(licenseKey);
+  console.log("--------------------------------------------\n");
 
-  if(licenseKey != "\0") {
-    res.render("test", { title: "Test Page - AQMesh API" });
+  // Check value of licenseKey
+  if(licenseKey !== "0") {
+    res.render("test", {
+      title: "Test Page - AQMesh API",
+      accountID: req.body.accountID,
+      licenseKey: req.body.licenseKey
+    });
     console.log("Redirected to /test");
+    console.log("The licenseKey is " + licenseKey + "..\n\n");
   }
+
   else {
     res.render("404", { title: "INCORRECT INFO" });
     console.log("INCORRECT INFO");
   }
+
+  console.log("--------------------------------------------\n");
+});
+
+app.get('/index', function (req, res) {
+  res.redirect('/');
+});
+
+app.get('/home', function (req, res) {
+  res.redirect('/');
 });
 
 app.get("/*", function(req, res) {
   res.render("404");
 });
 
+// ------------------------ END OF ROUTES ------------------------
+
+
 // Listen on designated port number
 app.listen(PORT, () => {
-  console.log("Listening on localhost:" + PORT + "\n\n" + "Press Ctrl+C to quit..")
+  console.log("Listening on localhost:" + PORT + "\n\n" + "Press Ctrl+C to quit..\n\n")
 });
